5c9e7ed2f21a69e1b002bb01925b4caed9834113
Juan Marcos <jncontatojn@gmail.com>
2023-07-28 19:50:35 -0300

    elevando a lista de times

--- organo/src/App.js
+++ organo/src/App.js
@@ -4,6 +4,44 @@ import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#57c278",
+      corSecundaria: "#d9f7e9",
+    },
+    {
+      nome: "Front End",
+      corPrimaria: "#82cffa",
+      corSecundaria: "#e8f8ff",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#a5d157",
+      corSecundaria: "#f0f8e2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "#e06b69",
+      corSecundaria: "#fde7e8",
+    },
+    {
+      nome: "UX e Design",
+      corPrimaria: "#db6ebf",
+      corSecundaria: "#fae9f5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "#ffba05",
+      corSecundaria: "#fff5d9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#ff8a29",
+      corSecundaria: "#ffeedf",
+    },
+  ];
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -19,9 +57,14 @@ function App() {
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
-      <Time nome="Programação" />
-      <Time nome="Frontend" />
-      <Time nome="Data Science" />
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }

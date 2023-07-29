5168936fb221dfa03591d72b66b17d882bc1aa45
Juan Marcos <jncontatojn@gmail.com>
2023-07-29 12:50:05 -0300

    mapeando colaboradores

--- organo/src/App.js
+++ organo/src/App.js
@@ -64,6 +64,9 @@ function App() {
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.time === time.nome
+          )}
         />
       ))}
     </div>
--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import "./Colaborador.css";
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img src="http://github.com/devjuanmarcos.png" alt="foto perfil git" />
+        <img src={imagem} alt={nome} />
       </div>
       <div className="rodape">
-        <h4>Juan Marcos</h4>
-        <h5>Estudante</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   );

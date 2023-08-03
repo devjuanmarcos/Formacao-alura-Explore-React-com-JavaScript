c91d43892df30ad6405031341e50f0539ab15ab7
Juan Marcos <jncontatojn@gmail.com>
2023-08-03 15:59:28 -0300

    creating router

--- 4-Curso-Alura-React-desenvolvendo-em React-Router-com-JavaScript/routes/src/App.js
+++ 4-Curso-Alura-React-desenvolvendo-em React-Router-com-JavaScript/routes/src/App.js
@@ -1,10 +1,32 @@
 import About from "./pages/About";
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 console.log(window.location);
 
 const page = window.location.pathname === "/" ? <Home /> : <About />;
 
 export default function App() {
-  return page;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path='/about'
+          element={<About />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path='*'
+          element={<div>Página não encontrada</div>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
